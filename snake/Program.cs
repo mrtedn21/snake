ac5a6caa0ade6ebaf1cd74476364d548e901bb8c
mrtedn21 <bezgin.sasha06@gmail.com>
2021-11-20 18:01:56 +0300

    wrap all code to namespace Application and class Program to avoid problems with declarations

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,105 +1,115 @@
-﻿Snake snake = new Snake();
-
-while (true)
+﻿namespace Application
 {
-    snake.move();
-    if (Console.KeyAvailable)
+    class Program
     {
-        ConsoleKeyInfo cki = Console.ReadKey();
-        snake.setVectorByKey(cki.Key);
-    }
-}
-
-enum Vector
-{
-    up,
-    down,
-    left,
-    right,
-}
-
-class Position
-{
-    public Position(int xParam, int yParam)
-    {
-        x = xParam;
-        y = yParam;
-    }
-    public int x { get; set; }
-    public int y { get; set; }
-}
-
-class Snake
-{
-    public Snake()
-    {
-        vector = Vector.right;
-
-        positions = new List<Position>();
-        positions.Add(new Position(0, 0));
-    }
-
-    public Vector vector { get; set; }
-    private List<Position> positions;
-
-    public void setVectorByKey(ConsoleKey key)
-    {
-        switch (key)
+        public static void Main()
         {
-            case ConsoleKey.RightArrow:
-                vector = Vector.right;
-                break;
+            Snake snake = new Snake();
 
-            case ConsoleKey.LeftArrow:
-                vector = Vector.left;
-                break;
+            while (true)
+            {
+                snake.move();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    snake.setVectorByKey(cki.Key);
+                }
+            }
 
-            case ConsoleKey.UpArrow:
-                vector = Vector.up;
-                break;
-
-            case ConsoleKey.DownArrow:
-                vector = Vector.down;
-                break;
         }
     }
 
-    public void move()
+    enum Vector
     {
-        Position oldHeadPosition = positions.Last();
-        int headX = oldHeadPosition.x;
-        int headY = oldHeadPosition.y;
+        up,
+        down,
+        left,
+        right,
+    }
 
-        switch (vector)
+    class Position
+    {
+        public Position(int xParam, int yParam)
         {
-            case Vector.up:
-                positions.Add(new Position(headX, headY - 1));
-                break;
+            x = xParam;
+            y = yParam;
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
 
-            case Vector.down:
-                positions.Add(new Position(headX, headY + 1));
-                break;
+    class Snake
+    {
+        public Snake()
+        {
+            vector = Vector.right;
 
-            case Vector.left:
-                positions.Add(new Position(headX - 1, headY));
-                break;
-
-            case Vector.right:
-                positions.Add(new Position(headX + 1, headY));
-                break;
+            positions = new List<Position>();
+            positions.Add(new Position(0, 0));
         }
 
-        // Draw new position of head
-        Position newHeadPosition = positions.Last();
-        Console.SetCursorPosition(newHeadPosition.x, newHeadPosition.y);
-        Console.WriteLine("X");
+        public Vector vector { get; set; }
+        private List<Position> positions;
 
-        //Clear old position of tail and delete tail
-        Position tailPosition = positions[0];
-        Console.SetCursorPosition(tailPosition.x, tailPosition.y);
-        Console.WriteLine(" ");
-        positions.RemoveAt(0);
+        public void setVectorByKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.RightArrow:
+                    vector = Vector.right;
+                    break;
 
-        Thread.Sleep(100);
+                case ConsoleKey.LeftArrow:
+                    vector = Vector.left;
+                    break;
+
+                case ConsoleKey.UpArrow:
+                    vector = Vector.up;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    vector = Vector.down;
+                    break;
+            }
+        }
+
+        public void move()
+        {
+            Position oldHeadPosition = positions.Last();
+            int headX = oldHeadPosition.x;
+            int headY = oldHeadPosition.y;
+
+            switch (vector)
+            {
+                case Vector.up:
+                    positions.Add(new Position(headX, headY - 1));
+                    break;
+
+                case Vector.down:
+                    positions.Add(new Position(headX, headY + 1));
+                    break;
+
+                case Vector.left:
+                    positions.Add(new Position(headX - 1, headY));
+                    break;
+
+                case Vector.right:
+                    positions.Add(new Position(headX + 1, headY));
+                    break;
+            }
+
+            // Draw new position of head
+            Position newHeadPosition = positions.Last();
+            Console.SetCursorPosition(newHeadPosition.x, newHeadPosition.y);
+            Console.WriteLine("X");
+
+            //Clear old position of tail and delete tail
+            Position tailPosition = positions[0];
+            Console.SetCursorPosition(tailPosition.x, tailPosition.y);
+            Console.WriteLine(" ");
+            positions.RemoveAt(0);
+
+            Thread.Sleep(100);
+        }
     }
 }
