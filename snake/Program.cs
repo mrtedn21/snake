ed5da5079d7cada9473ca51c693e4a05d5f7aa8a
sasha <sasha@DESKTOP-D60AMBA>
2021-11-20 06:48:15 +0300

    initial test code

---
+++ snake/Program.cs
@@ -0,0 +1,14 @@
+﻿for (int i = 0; i < 30; i++)
+{
+    if (i < 15)
+    {
+        Console.SetCursorPosition(i, i);
+    }
+    else
+    {
+        Console.SetCursorPosition(i, 30 - i);
+    }
+    
+    Console.Write("+");
+    Thread.Sleep(100);
+}
