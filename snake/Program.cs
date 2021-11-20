5adce5951994b249e0edec48a8fecd3cc0b07b78
mrtedn21 <bezgin.sasha06@gmail.com>
2021-11-20 07:39:30 +0300

    add key reading if key pressed and exit program if the key is "Enter"

--- snake/Program.cs
+++ snake/Program.cs
@@ -18,22 +18,35 @@
 
 int[] position = new int[2];
 
-for (int i = 0; i < 30; i++)
+while (true)
 {
-    position = detectPosition(i);
-
-    Console.SetCursorPosition(position[0], position[1]);
-    Console.Write("+");
-
-    position = detectPosition(i - 6);
-
-    try
+    for (int i = 0; i < 30; i++)
     {
-        Console.SetCursorPosition(position[0], position[1]);
-        Console.Write(" ");
-    }
-    catch (ArgumentOutOfRangeException)
-    { }
+        position = detectPosition(i);
 
-    Thread.Sleep(100);
+        Console.SetCursorPosition(position[0], position[1]);
+        Console.Write("+");
+
+        position = detectPosition(i - 6);
+
+        try
+        {
+            Console.SetCursorPosition(position[0], position[1]);
+            Console.Write(" ");
+        }
+        catch (ArgumentOutOfRangeException)
+        { }
+
+        Thread.Sleep(100);
+
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Enter)
+            {
+                return;
+            }
+        }
+        
+    }
 }
